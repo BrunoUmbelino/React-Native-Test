2b535cf063bb74a799677fd6943ddaf6bfa28136
Bruno <bruno.umbelino0@gmail.com>
2021-05-01 23:52:54 -0300

    setting environment

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableNativeFeedback,
+  Touchable,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  console.log("loaded");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button color="orange" title="Click me" />
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "lightblue" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
